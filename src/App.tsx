0c99eb14a1a8cd6376db4eab667a8cc610d281bc
Yugandhar Pathi <yugandhar.pathi@gmail.com>
2024-09-23 16:45:24 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <div style={{display: "flex", flexDirection:"column"}}>
+      <div style={{borderBottom:'1px solid black'}}>
+        <div style={{margin: '20px'}} >Header</div>
+      </div>
+      <iframe src="http://localhost:3000"></iframe>
+
+    </div>
+  )
+}
+
+export default App
