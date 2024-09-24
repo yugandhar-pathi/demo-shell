297b3952e39ecff64640a20fc0e1b6b07d64c635
Yugandhar Pathi <yugandhar.pathi@gmail.com>
2024-09-24 13:39:00 -0700

    add vercel toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 
+import { mountVercelToolbar } from '@vercel/toolbar/vite';
+
+
 const MICROAPP_URL = import.meta.env.VITE_MICROAPP_URL;
+
+mountVercelToolbar();
 
 function App() {
 
   return (
     <div style={{display: "flex", flexDirection:"column"}}>
       <div style={{borderBottom:'1px solid black'}}>
-        <div style={{margin: '20px'}} >Header</div>
+        <div style={{margin: '20px'}} >Shell</div>
       </div>
       <iframe src={MICROAPP_URL}></iframe>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { vercelToolbar } from '@vercel/toolbar/plugins/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercelToolbar()],
 })
